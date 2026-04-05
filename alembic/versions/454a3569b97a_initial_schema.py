bb6fc5acdf8184e544913f9adf2f8e5c6223f19b
Abhinav Mishra <mishraabhinav36@gmail.com>
2026-04-06 03:57:28 +0530

    release: v0.1.0

---
+++ alembic/versions/454a3569b97a_initial_schema.py
@@ -0,0 +1,28 @@
+"""initial schema
+
+Revision ID: 454a3569b97a
+Revises: 
+Create Date: 2026-04-05 11:21:00.763781
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '454a3569b97a'
+down_revision: Union[str, Sequence[str], None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass

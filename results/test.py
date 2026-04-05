38e5c305d6c1bab6942f46ceb60b611e3d9ab593
Abhinav Mishra <mishraabhinav36@gmail.com>
2026-04-05 12:08:43 +0530

    fix: remove unused scripts and outdated data files

--- results/test.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-# Read the CSV file
-csv="cured_morpho_seqs_v2.fa.result.csv"
-csv = pd.read_csv(csv)
-print(csv)
-print(csv.loc[(csv['Target Name']=='MAP2K2') & (csv['Kinase Name']=='MAPK3')])
-#print(csv.loc[(csv['Target Name']=='AKT1') & (csv['Kinase Name']=='MAPK3')])
